db739dcf3c6b6cf30deee1ef4d80d26fbc09b464
rak1204 <90257620+rak1204@users.noreply.github.com>
2021-09-10 23:18:43 +0400

    Update CST.java

--- CST.java
+++ CST.java
@@ -1,4 +1,26 @@
 
 public class CST {
+  
+  CST cantStopCST = null; 
+	
+  
+    public CST(String[] names, Color[] colors) 
+    {
+    	
+    	cantStopCST = new CST(names, colors);
+    }
+
+   
+    public CST getCST() 
+    {
+       
+    	return cantStopCST;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Util.showNewGameDialog("Choose player names and colors.");
+    }
 
 }
