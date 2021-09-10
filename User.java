6243e033dabb6a314ded1429f2e3e21c0c023741
rak1204 <90257620+rak1204@users.noreply.github.com>
2021-09-10 23:41:17 +0400

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,34 @@
 
 public class User {
+  
+	private String name = null;				
+	private char id = 'a';					
+	private Color color = Color.black;		
+	
+    
+    public User(String name, char id, Color color) 
+    {
+        this.name = name;
+        this.id = id;
+        this.color = color;
+    }
+
+    
+    public String getName() 
+    {
+        return name;
+    }
+
+    
+    public char getId() 
+    {
+        return id;
+    }
+
+   
+    public Color getColor() 
+    {
+        return color;
+    }
 
 }
